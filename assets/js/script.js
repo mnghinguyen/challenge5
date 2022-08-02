a7d2c7431c0ac86f6311897cdb2b089706ce1a67
Annie Nguyen <30813052+mnghinguyen@users.noreply.github.com>
2022-08-01 22:05:25 -0400

    define hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,19 @@
 var now = moment().format("MMM Do, YYYY");
 $("#currentDay").text(now);
+// Change background
+var hour = moment().format("a A");
+$(".hour").text("9AM");
+
+    function timeCheck() {
+    $("#timeCheck").addClass("present");
+    var timeCheck = moment().isBefore("HH");
+    if (timeCheck === true){
+        $("#timeCheck").addClass("past");} else if 
+        (timeCheck === false) {
+        $("#timeCheck").addClass("future");
+        } else {
+        return;
+        }
+}
+
+
